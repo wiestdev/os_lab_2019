5870bd45089b3bd52a8e06e938b123a70cf8e75c
wiestdev <145812260+wiestdev@users.noreply.github.com>
2023-12-01 04:52:00 +0000

    Update swap.c and rever_string.c

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,15 @@
 #include "revert_string.h"
+#include <string.h>
+#include <stdlib.h>
 
 void RevertString(char *str)
 {
-	// your code here
+	size_t n = strlen(str);
+	char *tmp_str = malloc(sizeof(char) * (n + 1));
+	strcpy(tmp_str, str);
+	for (int i = 0; i < n; i++) {
+		str[i] = tmp_str[n - i - 1];
+	}
+	free(tmp_str);
 }
 
